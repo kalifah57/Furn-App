4f47c51efec008f02dd9147c0e0b87c9ce3cbe06
kalifah57 <kalifah57@users.noreply.github.com>
2026-07-31 03:23:59 +0000

    deploy: 4795772feb3ea049ef8628c03aff566ea3cdfbb6

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3548048836"
+    serviceWorkerVersion: "2904291641"
   }
 });
